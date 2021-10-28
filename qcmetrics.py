54ae5415cf3a8c749b4f22de6523cdd68849b48d
Michael Nestor <47347033+mdnestor@users.noreply.github.com>
2021-10-28 12:42:51 -0700

    Updated PCA procedure

--- qcmetrics.py
+++ qcmetrics.py
@@ -4,33 +4,34 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def compute_pca(x, y):
+def compute_pca(x):
     from sklearn.preprocessing import StandardScaler
     from scipy.stats import zscore
     from scipy.linalg import eigh
     
     x = zscore(x)
-    A = np.matmul(x.T , x)
-
     k = x.shape[1]
-    values, vectors = eigh(A, eigvals=(k-2, k-1))
-    vectors = vectors.T
-    T = np.matmul(vectors, x.T)
-
-    T = np.vstack((T, y)).T
-    df = pd.DataFrame(data=T, columns=('PC1', 'PC2', 'label'))
-    df['label'] = df['label'].astype('category')
-    return df
+    cov_mat = np.cov(x , rowvar = False)
+    eigenvalues , eigenvectors = eigh(cov_mat, eigvals=(k-2, k-1))
+    #eigenvectors = eigenvectors.T
+    idx = np.argsort(eigenvalues)[::-1]
+    sorted_eigenvalues = eigenvalues[idx]
+    sorted_eigenvectors = eigenvectors[:,idx]
+    eigenvector_subset = sorted_eigenvectors[:,0:2]
+    z = np.dot(eigenvector_subset.T, x.T)
+    return z.T
         
 def plot_several_pca(datasets, y):
     fig, axs = plt.subplots(1, len(datasets), figsize=(6*len(datasets), 6))
+    y = np.expand_dims(y, 1)
     for i in range(len(datasets)):
         key = list(datasets)[i]
         x = datasets[key]
-        df = compute_pca(x, y)
-        ax = axs[i]
-        sns.scatterplot(x='PC1', y='PC2', data=df, hue='label', legend=False, ax=ax)
-        ax.set_title(key)
+        z = compute_pca(x)
+        df = pd.DataFrame(np.concatenate([z, y], axis=1), columns = ['PC1','PC2','batch'])
+        df['batch'] = df['batch'].astype('category')
+        sns.scatterplot(x='PC1', y='PC2', data=df, hue='batch', legend=False, ax=axs[i])
+        axs[i].set_title(key)
     return axs
 
 def plot_several_anova(datasets, y):
