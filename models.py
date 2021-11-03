a534d339953c0754bcf27ba11ab5fa249165fae8
Doo Nam Kim <doonam.kim@gmail.com>
2021-11-02 22:42:24 -0700

    models

--- models.py
+++ models.py
@@ -150,6 +150,9 @@ class NormAE(nn.Module):
                     if verbose: print('Early stopping after {} epochs'.format(epoch))
                     self.metrics = metrics
                     done = True
+        metrics = np.array(metrics)
+        return metrics
+
                     
     def plot_metrics(self, lambda_schedule):
         import matplotlib.pyplot as plt
